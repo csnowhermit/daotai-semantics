a0f9aa51b5972527cf6a5c57358bb352c94a017a
920164255@qq.com <920164255@qq.com>
2020-08-27 09:01:24 +0800

    update: 优化素材保存

--- face_detect_demo.py
+++ face_detect_demo.py
@@ -17,29 +17,15 @@ lock = threading.RLock()
 def Receive():
     # print("start Receive")
     # print("start Receive")
-    rtsp_url = "rtsp://admin:quickhigh123456@192.168.0.201/h264/ch1/sub/av_stream"
+    rtsp_url = "rtsp://admin:quickhigh123456@192.168.0.200/h264/ch1/sub/av_stream"
     # rtsp_url = 0
     cap = cv2.VideoCapture(rtsp_url)
-    # cap = getCap(input_webcam)
-    frame_interval = 3  # Number of frames after which to run face detection
-    fps_display_interval = 5  # seconds
-    frame_count = 0
-
-    start_time = time.time()
     while True:
         ret, frame = cap.read()
-        if (frame_count % frame_interval) == 0:  # 跳帧处理，解决算法和采集速度不匹配
-            if ret is True:
-                # q.put(frame)
-                lock.acquire()
-                frame_buffer.push(frame)
-                lock.release()
-            # Check our current fps
-            end_time = time.time()
-            if (end_time - start_time) > fps_display_interval:
-                frame_rate = int(frame_count / (end_time - start_time))
-                start_time = time.time()
-                frame_count = 0
+        if ret is True:
+            lock.acquire()
+            frame_buffer.push(frame)
+            lock.release()
 
 def percept():
     print("face_detect:", face_detect)
@@ -48,6 +34,7 @@ def percept():
         if frame_buffer.size() > 0:
             lock.acquire()
             frame = frame_buffer.pop()    # 每次拿最新的
+            tmp = frame.copy()
             lock.release()
 
             # print("frame:", type(frame), frame.shape)    # <class 'numpy.ndarray'> (480, 640, 3)，（高，宽，通道）
@@ -63,12 +50,14 @@ def percept():
                     w = right - left
                     h = bottom - top
                     cv2.rectangle(frame, (int(left), int(top)), (int(right), int(bottom)), (0, 0, 255), 2)
+                savefile = "D:/testData/origin/" + formatTimestamp(time.time(), format="%Y%m%d_%H%M%S", ms=True) + ".jpg"
+                cv2.imwrite(savefile, tmp)
+
+                savefile = "D:/testData/detect/" + formatTimestamp(time.time(), format="%Y%m%d_%H%M%S", ms=True) + ".jpg"
+                cv2.imwrite(savefile, frame)
 
             cv2.imshow("frame", frame)
             cv2.waitKey(1)
-
-            savefile = "D:/testData/" + formatTimestamp(time.time(), format="%Y%m%d_%H%M%S", ms=True) + ".jpg"
-            cv2.imwrite(savefile, frame)
 
 if __name__ == '__main__':
     p1 = threading.Thread(target=Receive)
