8b6053368850c28b9ea97d82b83d3e2376274fe9
920164255@qq.com <920164255@qq.com>
2020-09-18 17:46:57 +0800

    update: 添加手动心跳

--- percept_coming_cstack.py
+++ percept_coming_cstack.py
@@ -83,17 +83,17 @@ def percept():
     age_gender_model.load_weights(fpath)
 
     while True:
-        # if int(time.time()) % 10 == 0:    # 每5s手动发一次心跳，避免rabbit server自动断开连接。自动发心跳机制存在的问题：因rabbitmq有流量控制机制，会屏蔽掉自动心跳机制
-        #     heartbeatDict = {}
-        #     heartbeatDict["daotaiID"] = daotaiID
-        #     heartbeatDict["sentences"] = ""
-        #     heartbeatDict["timestamp"] = str(int(time.time()) * 1000)
-        #     heartbeatDict["intention"] = "heartbeat"  # 心跳
-        #
-        #     backstage_channel.basic_publish(exchange=backstage_EXCHANGE_NAME,
-        #                                     routing_key=backstage_routingKey,
-        #                                     body=str(heartbeatDict))
-        #     print("heartbeatDict:", heartbeatDict)
+        if int(time.time()) % 10 == 0:    # 每5s手动发一次心跳，避免rabbit server自动断开连接。自动发心跳机制存在的问题：因rabbitmq有流量控制机制，会屏蔽掉自动心跳机制
+            heartbeatDict = {}
+            heartbeatDict["daotaiID"] = daotaiID
+            heartbeatDict["sentences"] = ""
+            heartbeatDict["timestamp"] = str(int(time.time()) * 1000)
+            heartbeatDict["intention"] = "heartbeat"  # 心跳
+
+            backstage_channel.basic_publish(exchange=backstage_EXCHANGE_NAME,
+                                            routing_key=backstage_routingKey,
+                                            body=str(heartbeatDict))
+            print("heartbeatDict:", heartbeatDict)
 
         if frame_buffer.size() > 0:
             lock.acquire()
